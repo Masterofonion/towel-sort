30006fff87cab6c940839c8f746e20e197d9bf15
Viacheslav Maltsev <79205849+Masterofonion@users.noreply.github.com>
2021-02-19 12:51:46 +0300

    feat: add solition

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 
-// You should implement your task here.
+ //You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if ((typeof matrix) == 'undefined') {
+        return [];
+    } else if (matrix.length == 0) {
+        return [];
+    } else {
+        return matrix.reduce(function(preItem, item, index) {
+            if (index % 2 == 0) {
+                return preItem.concat(item);
+            } else {
+                return preItem.concat(item.reverse());
+            }
+        }, []); 
+    }
+
 }
+
+
+
+
